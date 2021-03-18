c9f9bcfbb5dc158401466441a332a474b002a1c0
akisora <akisora178@gmail.com>
2021-03-18 15:40:01 +0900

    update 21.03.18

--- src/Movie.js
+++ src/Movie.js
@@ -4,9 +4,9 @@ function Movie({id, year, title, summary, poster}){
     return  (
                 <div className="contents_box">
                     <h3 className="movie_title">{title}</h3>
-                    <p className="movie_year">{year}</p>
+                    <p className="movie_year mt5">{year}</p>
                     <img className="movie_poster" src={poster} />
-                    <p class="desc movie_desc">{summary}</p>
+                    <p class="desc movie_desc mt10">{summary}</p>
                 </div>);
 }
 
